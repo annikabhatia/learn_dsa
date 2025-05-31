4cbf61c25598cef309a78689b3234479ad5f809f
Annika <36318224+annikabhatia@users.noreply.github.com>
2025-05-30 22:16:43 -0400

    Update suq_tc1.py

--- stack/StackUsingQueue/suq_tc1.py
+++ stack/StackUsingQueue/suq_tc1.py
@@ -1,1 +1,12 @@
+stack = MyStack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
 
+assert stack.top() == 30
+assert stack.pop() == 30
+assert stack.top() == 20
+assert stack.pop() == 20
+assert stack.empty() == False
+assert stack.pop() == 10
+assert stack.empty() == True
