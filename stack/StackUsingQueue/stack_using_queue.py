92a5ee7c1e202f31c1f5efcf86eb62602745bde3
Annika <36318224+annikabhatia@users.noreply.github.com>
2025-05-30 22:26:38 -0400

    Create stack_using_queue.py

---
+++ stack/StackUsingQueue/stack_using_queue.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class MyStack:
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+    
+    def push(self, x):
+        # implement this
+        pass
+
+    def pop(self):
+        # implement this
+        pass
+
+    def top(self):
+        # implement this
+        pass
+
+    def empty(self):
+        # implement this
+        pass
