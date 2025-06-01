be9ccb39f86880b84abb9ae464329fbb6c14b921
Annika <annika.bhatia@gmail.com>
2025-06-01 14:38:56 -0400

    code changes

--- stack/StackUsingQueue/stack_using_queue.py
+++ stack/StackUsingQueue/stack_using_queue.py
@@ -6,17 +6,29 @@ class MyStack:
         self.q2 = deque()
     
     def push(self, x):
-        # implement this
-        pass
+        self.q1.append(x)
+    
+        for i in self.q1:
+            #removes/returns the leftmost element of the deque
+            self.q2.append(self.q1.popleft())
+    
+        #swap 2 queues so q1 can be real stack
+        temp = self.q1
+        self.q1 = self.q2
+        self.q2 = temp
 
     def pop(self):
-        # implement this
-        pass
+        #Remove element from top of the stack
+        return self.q1.popleft()
+
 
     def top(self):
-        # implement this
-        pass
+        #Returns the element on the top of the stack without removing it.
+        return(self.q1[0])
 
     def empty(self):
-        # implement this
-        pass
+        #Returns `true` if the stack is empty, `false` otherwise.
+        if len(self.q1) == 0:
+            return True
+        else:
+            return False 
--- stack/next_greater/next_greator.py
+++ stack/next_greater/next_greator.py
@@ -1,8 +1,16 @@
 from typing import List
 
 def next_greater_element(arr: List[int]) -> List[int]:
-    # Your code here
-    pass
+    stack = []
+    for i in arr:
+        #condition should be that we need to reach 2nd element some how...so maybe just ignore first element?
+        while arr
+            stack.append(i)
+        if i - len(arr) == 0:
+            stack.append(-1)
+    return stack
+
+
 
 if __name__ == "__main__":
     sample = [4, 5, 2, 10]
