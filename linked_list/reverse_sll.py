e895a6e161852326603d7f4f8399f7f269fa913b
Annika <annika.bhatia@gmail.com>
2025-07-31 23:43:27 -0400

    commit

--- linked_list/reverse_sll.py
+++ linked_list/reverse_sll.py
@@ -1,1 +1,13 @@
 #Reverse a singly linked list.
+
+def reverse_sll(self):
+    prev=None
+    current = self.head
+    
+    while current:
+        next_node=current.next
+        current.next=prev
+        prev=current
+        current=next_node
+    
+    self.head=prev
--- linked_list/singly_linked_list.py
+++ linked_list/singly_linked_list.py
@@ -1,9 +1,37 @@
 #Build basic singly linked list.
+
 class Node:
-     def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, data):
+        self.data = data  #storing data
+        self.next = None  #next part of node
 
-def insert(head, data):
 
-def traverse(head):
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    #insert node to end of the list
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head: 
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:  
+            current = current.next
+        current.next = new_node
+
+    def print(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+    def search(self, key):
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
